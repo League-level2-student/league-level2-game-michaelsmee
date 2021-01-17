8cdf008009bba8f26819701d93b5f97fcb3f2d7a
root <root@d97d317945ef>
2021-01-17 20:57:54 +0000

    I am working on the game ideas now

---
+++ src/Game.java
@@ -0,0 +1,32 @@
+/*
+ * There will be a set of 2 trees, one above and one below at random height, allowing a gap
+ * of around 300-400ish space in between them
+ * you will be a bark speeder that can go up or down
+ * the trees will gradually come forward to you at the same time
+ * one the trees reach a x value of 0, they de-spawn
+ * after 50 pair of trees, a grey rectangle (may be changed later)
+ * go into the grey rectangle to end the game
+ * 
+ * Classes:
+ * main:
+ * object manager:
+ * trees:makes a random number
+ * use this number for a tree length
+ * modify the image
+ * or make it just a green rectangle
+ * bark speeder:
+ * will make the bark speeder
+ * have its movement and how it can fire
+ * has 3 health
+ * main character
+ * game panel:
+ * game object:
+ * projectile:
+ * controls both your and the enimies projectiles
+ * enemy classs:
+ * would have a human who will shoot a bullet rarely 
+ * has 1 health, moves forward
+ */
+public class Game {
+
+}
