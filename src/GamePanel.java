c088c3368595ff52ac4298b2fa1a8817374439b9
root <root@d97d317945ef>
2021-01-31 20:49:18 +0000

    working on game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,22 @@
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel {
+import javax.swing.JPanel;
+
+public class GamePanel  extends JPanel {
+
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+	int currentState = MENU;
+	
+	void updateMenuState() {}
+	void updateGameState() {}
+	void updateEndState() {}
+@Override
+public void paintComponent (Graphics g) {
+	g.fillRect(10,10,100,100);
+}
 
 }
--- src/SpeederGame.java
+++ src/SpeederGame.java
@@ -36,16 +36,17 @@
  * 		has 1 health, moves forward at a constant rate
  */
 import java.awt.Graphics;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class SpeederGame {
-	
-
-		JFrame frame; 
-		GamePanel panel;
-		public static final int WIDTH = 800;
-		public static final int HEIGHT = 500; 
+public class SpeederGame{
+	JFrame frame; 
+	GamePanel panel;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 500; 
 
 		public static void main(String[] args) {
 			SpeederGame speederGame = new SpeederGame();
@@ -57,7 +58,7 @@ public class SpeederGame {
 			frame.setVisible(true);
 			panel = new GamePanel();
 			frame.add(panel);
-			frame.addKeyListener(panel);
+			
 		}
 
 		void setup() {
@@ -67,4 +68,4 @@ public class SpeederGame {
 		}
 
 	}
-}
+
